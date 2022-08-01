8d1f6e714c8a645099201e1199696663f9689c76
user <arshadahamad525@gmail.com>
2022-08-01 18:32:56 +0530

    08/01

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountManagementComponent } from './component/account-management/account-management.component';
 import { AssteManagementComponent } from './component/asste-management/asste-management.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { LandingBannerComponent } from './component/landing-banner/landing-banner.component';
 import { LandingPageComponent } from './component/landing-page/landing-page.component';
+import { SchedulingAndAlertComponent } from './component/scheduling-and-alert/scheduling-and-alert.component';
+import { UserManagementComponent } from './component/user-management/user-management.component';
 import { WorkOrderComponent } from './component/work-order/work-order.component';
 
 const routes: Routes = [
@@ -13,6 +16,9 @@ const routes: Routes = [
   {path: 'asset', component: AssteManagementComponent},
   {path: 'workOrder', component: WorkOrderComponent},
   {path: 'dashboard', component: DashboardComponent},
+  {path: 'user', component: UserManagementComponent},
+  {path: 'alert', component: SchedulingAndAlertComponent},
+  {path: 'account', component: AccountManagementComponent},
 ];
 
 @NgModule({
